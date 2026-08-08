33bbb5b54bd774e143dd522c730f23c3754e0c49
Dandy Purwanto <dandyekowork@gmail.com>
2026-08-08 22:15:43 +0700

    Fix CI authentication environment variables

--- config/environment.py
+++ config/environment.py
@@ -7,20 +7,17 @@ load_dotenv(
     override=True
 )
 
-
 def get_base_url():
 
     return os.getenv(
         "BASE_URL"
     )
 
-
 def get_username():
 
     return os.getenv(
         "USERNAME"
     )
-
 
 def get_password():
 
